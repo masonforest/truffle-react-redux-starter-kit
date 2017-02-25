57d944c047f244372c239e78e6c5f09c2d0c1d89
Mason Fischer <mason@thoughtbot.com>
2017-02-25 13:26:21 -0600

    merge app and src

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>MetaCoin</h1>
+        <h2>Example Truffle Dapp</h2>
+        <h3>You have <span className="black"><span id="balance"></span> META</span></h3>
+
+        <br />
+        <h1>Send</h1>
+        <br /><label htmlFor="amount">Amount:</label><input type="text" id="amount" placeholder="e.g., 95"></input>
+        <br /><label htmlFor="receiver">To Address:</label><input type="text" id="receiver" placeholder="e.g., 0x93e66d9baea28c17d9fc393b53e3fbdd76899dae"></input>
+        <br /><br /><button id="send" onClick={() => {}}>Send MetaCoin</button>
+        <br /><br />
+        <span id="status"></span>
       </div>
     );
   }
